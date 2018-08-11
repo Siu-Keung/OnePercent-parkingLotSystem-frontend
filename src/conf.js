a8716389d5c51469dd25563535ae377606bdc059
SiuKeung <siu_keung@outlook.com>
2018-08-11 12:54:14 +0800

    No Comment

--- src/conf.js
+++ src/conf.js
@@ -1,6 +1,6 @@
 export default {
     // frontendHost: 'http://localhost:9000',
     frontendHost: 'https://parkinglotsystem.herokuapp.com',
-    domain:"http://localhost:1234"
-    // domain:'https://parking-lot-system-backend.herokuapp.com'
+    // domain:"http://localhost:1234"
+    domain:'https://parking-lot-system-backend.herokuapp.com'
 }
